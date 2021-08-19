d7dc85741b3503bd189bc02e76abecd8c86d9a89
Igor <ermygut@gmail.com>
2021-08-19 16:36:21 +0300

    new expense form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
-
-function App() {
+//arrow syntax  allowed
+const App = () => {
 	const expenses = [
 		{
 			id: 'e1',
@@ -27,10 +27,11 @@ function App() {
 
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense />
+
 			<Expenses items={expenses} />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props) {
+//we can put the export command before the function declaration
+export default function ExpenseItem(props) {
+	const [title, setTitle] = useState(props.title);
+
+	const clickHandler = () => {
+		setTitle('hhhh');
+		console.log(title);
+	};
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change title</button>
 		</Card>
 	);
 }
-
-export default ExpenseItem;
