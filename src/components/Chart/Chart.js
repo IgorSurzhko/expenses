c149142e01a0db0ee2021ec68d508360c603afd5
Igor <ermygut@gmail.com>
2021-08-21 17:41:20 +0300

    small bug fix

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,10 +3,19 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = props => {
+	//maps/transforms dataPoint array to array of numbers
+	const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+	//Math.max requires standalone numbers, so spread operator helps us
+	const totalMaximum = Math.max(...dataPointValues);
 	return (
 		<div className="chart">
 			{props.dataPoints.map(dataPoint => (
-				<ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label} />
+				<ChartBar
+					key={dataPoint.label}
+					value={dataPoint.value}
+					maxValue={totalMaximum}
+					label={dataPoint.label}
+				/>
 			))}
 		</div>
 	);
